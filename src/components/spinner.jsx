5def26ef7490c336aa2dc296ae2163287aa4c6d3
rksab <ramansarabha@gmail.com>
2025-08-27 01:34:20 +0530

    fixed spinner

---
+++ src/components/spinner.jsx
@@ -0,0 +1,8 @@
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-6 h-6 border-4 border-gray-500 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
+}
+export default Spinner; 
